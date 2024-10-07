124004e0001c1ab289e7a2ee193c33134aabbded
surbhi-garg <surbhi.garg@tothenew.com>
2024-10-08 00:09:48 +0530

    OV-1756: Changes to support jakarta standards and comply with java 21

--- src/main/java/in/zapr/druid/druidry/client/DruidJerseyClient.java
+++ src/main/java/in/zapr/druid/druidry/client/DruidJerseyClient.java
@@ -16,6 +16,7 @@
 
 package in.zapr.druid.druidry.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.reflect.TypeUtils;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -25,19 +26,18 @@ import org.glassfish.jersey.client.ClientConfig;
 
 import java.util.List;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import in.zapr.druid.druidry.client.exception.ConnectionException;
 import in.zapr.druid.druidry.client.exception.QueryException;
 import in.zapr.druid.druidry.query.DruidQuery;
 import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class DruidJerseyClient implements DruidClient {
